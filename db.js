baff87dd8d44ea15300a78fbfc161bec22fadc34
Mohamed Elsaeed <mohamed.smenshawy@gmail.com>
2023-07-20 20:34:35 +0300

    databases edited

--- db.js
+++ db.js
@@ -1,22 +1,60 @@
-import { Pool } from 'pg'
+import { Client } from "pg";
 
-const pool = new Pool({
-    user : 'postgres',
-    password : 'RDTMGP1',
-    host : 'localhost',
-    port : '5432',
-    database : 'formdb'
+const client = new Client({
+  user: 'postgres',
+  host : 'localhost',
+  database: 'formdb',
+  password : 'RDTMGP1',
+  port: 5432,
 })
 
 
-// let localPoolConfig   = {
+export default client ;
+
+
+
+
+
+
+
+
+
+
+// // const { Client } = require('pg')
+// const connectionString = 'postgresql://postgres:RDTMGP1@localhost:5432/formdb' ;
+// // const client = new Client({
+// //     connectionString,
+// //   }) ;
+
+//  client.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+
+// export default client  ;
+
+
+// const client = new Client({
+//   user: 'postgres',
+//   host : 'localhost',
+//   database: 'formdb',
+//   password : 'RDTMGP1',
+//   port: 5432,
+// })
+//-------------------------------------
+
+// import { Pool } from 'pg'
+
+
+
+// // let localPoolConfig   = {
     
-// };
+// // };
 
-const poolConfig = process.env.DATABASE_URL ? {
-    connectionString: process.env.DATABASE_URL,
-    ssl:{rejectUnauthorized : false}} : localConfig;
+// const poolConfig = process.env.DATABASE_URL ? {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl:{rejectUnauthorized : false}} : localConfig;
 
-// const pool = new Pool(localPoolConfig);
-// export default pool;
+// // const pool = new Pool(localPoolConfig);
+// // export default pool;
 
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,6 +2,9 @@ import express from 'express';
 import pool from '../db.js';
 
 const router = express.Router();
+
+router.get('/')
+
 router.get('/',async (req,res) => {
     try{
         const users = await pool.query('Select * from users');
