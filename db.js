55e64ab4d66429b7c8616da26505621552cd98e3
Rokia Darwish <94113862+Rokia-DSC@users.noreply.github.com>
2023-07-20 20:36:37 +0300

    "2nd version

--- db.js
+++ db.js
@@ -6,17 +6,19 @@ const pool = new Pool({
     host : 'localhost',
     port : '5432',
     database : 'formdb'
-})
+});
+
+const connectionString = 'postgresql://postgres:RDTMGP1@localhost:5432/formdb'
 
 
-// let localPoolConfig   = {
+// // let localPoolConfig   = {
     
-// };
+// // };
 
-const poolConfig = process.env.DATABASE_URL ? {
-    connectionString: process.env.DATABASE_URL,
-    ssl:{rejectUnauthorized : false}} : localConfig;
+// const poolConfig = process.env.DATABASE_URL ? {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl:{rejectUnauthorized : false}} : localConfig;
 
-// const pool = new Pool(localPoolConfig);
-// export default pool;
+// // const pool = new Pool(localPoolConfig);
+// // export default pool;
 
